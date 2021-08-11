03d4a619071b293f874bb916d970dc971f3b4b05
Felipe Bernardes <84930952+Feliperazz@users.noreply.github.com>
2021-08-10 23:40:50 -0300

    Aula10

---
+++ quinzena2/funcoes-arrays/index.js
@@ -0,0 +1,83 @@
+// exercicio interpretação de codigo
+
+
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" }
+//   ]
+  
+//   const novoArrayA = usuarios.map((item, index, array) => {
+//      console.log(item, index, array)
+//   })
+  // vai imprimir o objeto "ususarios"
+
+//   const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayB = usuarios.map((item, index, array) => {
+//      return item.nome
+//   })
+  
+//   console.log(novoArrayB)
+
+  // nao entendi bem, rodei no console e ainda nao entendi pq 
+
+//   const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayC = usuarios.filter((item, index, array) => {
+//      return item.apelido !== "Chijo"
+//   })
+  
+//   console.log(novoArrayC)
+  // ele imprime o item que tiver diferente de "chijo"
+
+  // exercicio de escrita de codigo
+
+  const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},]
+
+    const arrayPets = pets.map((item) => {
+        return item.nome
+       
+    })
+    const arrayPets2 = pets.filter((item) => {
+        return item.raca === "Salsicha"
+    })
+    
+   
+   
+    console.log(arrayPets)
+    console.log(arrayPets2)
+    // console.log(`Você ganhou um cupom de desconto de 10% para tosar o/a ${arrayPets3}`)
+
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+ function nomeProdutos(item ) {
+     return item.nome
+ }
+ const arrayProdutos = produtos.map((nomeProdutos))
+
+ console.log(arrayProdutos)
